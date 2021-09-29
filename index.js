9bc2c30e2148db508071039326071c6b234a43af
nluuarch@gmail.com <nluuarch@gmail.com>
2021-09-28 22:46:31 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+
+const employee = {
+    name:"Nancy",
+    streetAddress:"854 Sargent",
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+};
+
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
